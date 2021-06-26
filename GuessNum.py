a3ca6ff6aa100db19b54be8341870355b85fa49a
Justin Van Den Hurk <Justin.t.hurk@gmail.com>
2021-06-27 00:05:06 +0930

    Initial commit

---
+++ GuessNum.py
@@ -0,0 +1,8 @@
+import random
+
+print("What number am I thinking of?")
+
+#userInput = input()
+randomNum = random.randint(0, 10)
+
+print()
---
+++ notes.md
@@ -0,0 +1,7 @@
+Guess the number game
+
+-user iput
+-random number generate
+-easy 1 to 10
+-medium 1 to 100
+-hard 1 to 1000
